7f710d8d497aca88385a9ae24cad0f0c0e1fc6ca
Andrey Nikolaev <a.nikolayev@msocialproduction.com>
2023-09-17 22:36:09 +0300

    #add #test - add item

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,15 @@ import App from './App';
 import '@testing-library/jest-dom/extend-expect';
 import { screen } from '@testing-library/react';
 
+const task1 = 'Задача-1';
+const task2 = 'Задача-2';
+
 describe('TodoList', () => {
-  it('удаляет завершенные задачи при нажатии на кнопку "Очистить завершенные"', () => {
+  it('добавить новую задачу', () => {
     render(<App />);
 
     const todoFormInput = screen.getByTestId('todo-form-input');
     const todoSubmitBtn = screen.getByTestId('todo-submit-btn');
-    const todoClearBtn = screen.getByTestId('todo-clear-btn');
-
-    const task1 = 'Задача-1';
-    const task2 = 'Задача-2';
 
     fireEvent.change(todoFormInput, {
       target: { value: task1 },
@@ -23,9 +22,33 @@ describe('TodoList', () => {
     fireEvent.change(todoFormInput, { target: { value: task2 } });
     fireEvent.click(todoSubmitBtn);
 
-    const todoItemBtn = screen.getByTestId(task1);
+    // Проверка, что  задача 1 в списке
+    expect(screen.getByTestId(task1)).toBeInTheDocument();
+    // Проверка, что  задача 2 в списке
+    expect(screen.getByTestId(task2)).toBeInTheDocument();
+  });
 
-    fireEvent.click(todoItemBtn);
+  it('переключение статуса задач и удаление завершенных при нажатии на кнопку "Очистить завершенные"', () => {
+    render(<App />);
+
+    const todoFormInput = screen.getByTestId('todo-form-input');
+    const todoSubmitBtn = screen.getByTestId('todo-submit-btn');
+    const todoClearBtn = screen.getByTestId('todo-clear-btn');
+
+    fireEvent.change(todoFormInput, {
+      target: { value: task1 },
+    });
+    fireEvent.click(todoSubmitBtn);
+
+    fireEvent.change(todoFormInput, { target: { value: task2 } });
+    fireEvent.click(todoSubmitBtn);
+
+    const todoItemBtn1 = screen.getByTestId(task1);
+    const todoItemBtn2 = screen.getByTestId(task2);
+
+    fireEvent.click(todoItemBtn1);
+    fireEvent.click(todoItemBtn2);
+    fireEvent.click(todoItemBtn2);
     fireEvent.click(todoClearBtn);
 
     // Проверка, что завершенная задача удалена из списка
